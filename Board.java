1ca8f94bab6268ef2b46e46dd6c80fd1e7407603
gyang0 <107653576+gyang0@users.noreply.github.com>
2022-12-28 21:58:52 -0800

    Update Board.java

--- Board.java
+++ Board.java
@@ -10,24 +10,31 @@ import java.awt.event.MouseListener;
 
 public class Board extends JComponent implements MouseListener {
     private Square[][] squares;
-    private char boardState[][] = {
-            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
-            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-            {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+    private Piece[][] pieces;
+
+    // First char is for what side (white or black), second char is for the piece (N for knight, R for rook, etc.)
+    private String boardState[][] = {
+            {"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"},
+            {"bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"},
+            {"", "", "", "", "", "", "", ""},
+            {"", "", "", "", "", "", "", ""},
+            {"", "", "", "", "", "", "", ""},
+            {"", "", "", "", "", "", "", ""},
+            {"wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"},
+            {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"}
     };
 
     private final int NUM_SQUARES = 8;
     private final int SQUARE_WIDTH = 50;
+    private final int X_OFFSET = 100;
+    private final int Y_OFFSET = 100;
+
     private final Color WHITE = new Color(0xFFFFFF);
     private final Color BLACK = new Color(0x000000);
 
-    private final int x = 100;
-    private final int y = 100;
+    private int numClicks = 0;
+    private int[] prevCoords = {-1, -1};
+    private String prevPieceType = "";
 
     public void initSquares(){
         squares = new Square[NUM_SQUARES][NUM_SQUARES];
@@ -37,9 +44,9 @@ public class Board extends JComponent implements MouseListener {
         for(int i = 0; i < NUM_SQUARES; i++){
             for(int j = 0; j < NUM_SQUARES; j++){
                 if(flag)
-                    squares[i][j] = new Square(x + i * SQUARE_WIDTH, y + j * SQUARE_WIDTH, SQUARE_WIDTH, WHITE);
+                    squares[i][j] = new Square(X_OFFSET + i * SQUARE_WIDTH, Y_OFFSET + j * SQUARE_WIDTH, SQUARE_WIDTH, WHITE);
                 else
-                    squares[i][j] = new Square(x + i * SQUARE_WIDTH, y + j * SQUARE_WIDTH, SQUARE_WIDTH, BLACK);
+                    squares[i][j] = new Square(X_OFFSET + i * SQUARE_WIDTH, Y_OFFSET + j * SQUARE_WIDTH, SQUARE_WIDTH, BLACK);
 
                 // Carrying on the color if at the end of the column
                 if(j != NUM_SQUARES - 1)
@@ -47,6 +54,7 @@ public class Board extends JComponent implements MouseListener {
             }
         }
     }
+
 
     /*
     public void placePiece(int i, int j, char pieceType){
@@ -82,9 +90,13 @@ public class Board extends JComponent implements MouseListener {
     }*/
 
     public void initPieces(){
-        /*
-        * Set squares[i][j].curPiece to something of type "Piece."
-        * */
+        pieces = new Piece[NUM_SQUARES][NUM_SQUARES];
+
+        for(int i = 0; i < NUM_SQUARES; i++){
+            for(int j = 0; j < NUM_SQUARES; j++){
+                pieces[i][j] = new Piece(i * SQUARE_WIDTH + X_OFFSET, j * SQUARE_WIDTH + Y_OFFSET, boardState[i][j]);
+            }
+        }
     }
 
     public Board(){
@@ -92,23 +104,21 @@ public class Board extends JComponent implements MouseListener {
         this.initPieces();
 
 
-        // Adds mouse listener
+        // Add mouse listener
         addMouseListener(this);
     }
 
-    public Square[][] getSquares(){
-        return this.squares;
-    }
 
     @Override
     public void paintComponent(Graphics g) {
         // Board outline
         g.setColor(BLACK);
-        g.fillRoundRect(this.x - 10, this.y - 10, NUM_SQUARES*SQUARE_WIDTH + 20, NUM_SQUARES*SQUARE_WIDTH + 20, 3, 3);
+        g.fillRoundRect(X_OFFSET - 10, Y_OFFSET - 10, NUM_SQUARES*SQUARE_WIDTH + 20, NUM_SQUARES*SQUARE_WIDTH + 20, 3, 3);
 
         for (int i = 0; i < NUM_SQUARES; i++){
             for (int j = 0; j < NUM_SQUARES; j++) {
                 squares[i][j].paint(g);
+                pieces[i][j].paint(g);
             }
         }
     }
@@ -120,10 +130,32 @@ public class Board extends JComponent implements MouseListener {
         for(int i = 0; i < NUM_SQUARES; i++){
             for(int j = 0; j < NUM_SQUARES; j++){
                 if(squares[i][j].contains(e.getX(), e.getY())){
-                    squares[i][j].selectCurSquare();
+                    // Second click
+                    if(numClicks == 1){
+                        numClicks = 0;
+                        pieces[i][j].setPiece(i * SQUARE_WIDTH + X_OFFSET, j * SQUARE_WIDTH + Y_OFFSET, prevPieceType);
+                        pieces[prevCoords[0]][prevCoords[1]].setPiece(prevCoords[0] * SQUARE_WIDTH + X_OFFSET,
+                                                                      prevCoords[1] * SQUARE_WIDTH + Y_OFFSET,
+                                                                    "");
+
+                        squares[i][j].deselectSquare();
+                        squares[prevCoords[0]][prevCoords[1]].deselectSquare();
+
+                        // Reset
+                        prevCoords[0] = -1;
+                        prevCoords[1] = -1;
+
+                    } else {
+                        numClicks++;
+                        prevCoords[0] = i;
+                        prevCoords[1] = j;
+                        prevPieceType = pieces[i][j].getType();
+
+                        squares[i][j].selectSquare();
+                    }
+
                     repaint();
                 }
-
             }
         }
 
