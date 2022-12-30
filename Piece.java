ee69f504dc527e2b00f8f42094d575050995d233
gyang0 <107653576+gyang0@users.noreply.github.com>
2022-12-30 04:49:33 -0800

    Update Piece.java

--- Piece.java
+++ Piece.java
@@ -11,19 +11,38 @@ import java.awt.*;
 
 public class Piece {
     private String type;
+
+    // this.x and this.y are the displaying coordinates.
     private int x;
     private int y;
-    private static final String PIECE_FONT = "Helvetica Neue-bold-30";
+
+    // this.gridX and this.gridY are the grid-type coordinates ( Like (0, 1), (1, 1), etc.).
+    private int gridX;
+    private int gridY;
+    private static final String PIECE_FONT = "Helvetica Neue-bold-40";
+    private final int NUM_SQUARES = 8;
+
+    private final int SQUARE_WIDTH = 50;
+    private final int X_OFFSET = 100;
+    private final int Y_OFFSET = 100;
 
     public Piece(int i, int j, String type){
-        this.x = i;
-        this.y = j;
+        this.x = j * SQUARE_WIDTH + X_OFFSET;
+        this.y = i * SQUARE_WIDTH + Y_OFFSET;
+
+        this.gridX = i;
+        this.gridY = j;
+
         this.type = type;
     }
 
     public void setPiece(int x, int y, String type){
-        this.x = x;
-        this.y = y;
+        this.x = y * SQUARE_WIDTH + X_OFFSET;
+        this.y = x * SQUARE_WIDTH + Y_OFFSET;
+
+        this.gridX = x;
+        this.gridY = y;
+
         this.type = type;
     }
 
@@ -35,20 +54,20 @@ public class Piece {
         g.setColor(Color.YELLOW);
         g.setFont(Font.decode(PIECE_FONT));
 
-        if(this.type.equals("wp")) g.drawString("\u2659", this.x, this.y);
-        else if(this.type.equals("wr")) g.drawString("\u2656", this.x, this.y);
-        else if(this.type.equals("wn")) g.drawString("\u2658", this.x, this.y);
-        else if(this.type.equals("wb")) g.drawString("\u2657", this.x, this.y);
-        else if(this.type.equals("wq")) g.drawString("\u2655", this.x, this.y);
-        else if(this.type.equals("wk")) g.drawString("\u2654", this.x, this.y);
+        if(this.type.equals("wp")) g.drawString("\u2659", this.x, this.y + 40);
+        else if(this.type.equals("wr")) g.drawString("\u2656", this.x, this.y + 40);
+        else if(this.type.equals("wn")) g.drawString("\u2658", this.x, this.y + 40);
+        else if(this.type.equals("wb")) g.drawString("\u2657", this.x, this.y + 40);
+        else if(this.type.equals("wq")) g.drawString("\u2655", this.x, this.y + 40);
+        else if(this.type.equals("wk")) g.drawString("\u2654", this.x, this.y + 40);
 
-        else if(this.type.equals("bp")) g.drawString("\u265F", this.x, this.y);
-        else if(this.type.equals("br")) g.drawString("\u265C", this.x, this.y);
-        else if(this.type.equals("bn")) g.drawString("\u265E", this.x, this.y);
-        else if(this.type.equals("bb")) g.drawString("\u265D", this.x, this.y);
-        else if(this.type.equals("bq")) g.drawString("\u265B", this.x, this.y);
-        else if(this.type.equals("bk")) g.drawString("\u265A", this.x, this.y);
-        else g.drawString("", this.x, this.y);
+        else if(this.type.equals("bp")) g.drawString("\u265F", this.x, this.y + 40);
+        else if(this.type.equals("br")) g.drawString("\u265C", this.x, this.y + 40);
+        else if(this.type.equals("bn")) g.drawString("\u265E", this.x, this.y + 40);
+        else if(this.type.equals("bb")) g.drawString("\u265D", this.x, this.y + 40);
+        else if(this.type.equals("bq")) g.drawString("\u265B", this.x, this.y + 40);
+        else if(this.type.equals("bk")) g.drawString("\u265A", this.x, this.y + 40);
+        else g.drawString("", this.x, this.y + 40);
     }
 
 
