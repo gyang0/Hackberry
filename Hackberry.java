8db96aeb22ec3f9e1d2099370bba842031b9ca17
gyang0 <107653576+gyang0@users.noreply.github.com>
2023-01-16 21:40:01 -0800

    Update Hackberry.java

--- Hackberry.java
+++ Hackberry.java
@@ -3,7 +3,6 @@
  * Jan 8, 2023 - All legal moves are now recognized (hopefully), including: En passant, castling, checks, promotion, etc.
  * **/
 
-
 /**
  * This is where most of the stuff will take place.
  * All methods will be combined to set turns, make moves, etc.
@@ -12,7 +11,19 @@
  *
  * TODO: Javadoc comments
  * TODO: Different side toggle
- * TODO: Fix new bugs
+ * TODO: save games to PGN file format
+ * TODO: if two knights can move to the same square, then the notation should reflect that.
+ * TODO: all (or at least most) illegal moves should be prohibited in the this.canMoveTo method.
+ * TODO: illegal moves shouldn't be highlighted squares.
+ *
+ * TODO: Note that my HashMap of pieces should never delete a piece unless it's captured. Instead, it should set that piece.
+ *
+ * I'll need to have a separate method or something, that goes through every move in the HashMap and tries it out.
+ * Then if it causes the king to get in check, I'll make it not possible.
+ * Remember: in this method I'm tampering with blackKingInCheck and whiteKingInCheck directly.
+ * So I'll need to be careful about what my final value is for those.
+ * If it was false initially, it should be false after that too.
+ * It should also be reset for every iteration.
  * **/
 
 import javax.swing.*;
