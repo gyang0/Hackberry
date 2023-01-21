6abd3397d6c2e0676cd6f2bda321e81d37d93aea
gyang0 <107653576+gyang0@users.noreply.github.com>
2023-01-21 06:17:32 -0800

    Update Hackberry.java

--- Hackberry.java
+++ Hackberry.java
@@ -1,3 +1,4 @@
+
 /** Milestones
  * Jan 4, 2023 - Successfully recreated the Opera Game (Paul Morphy vs Duke of Brunswick & Count Isouard, 1858).
  * Jan 8, 2023 - All legal moves are now recognized (hopefully), including: En passant, castling, checks, promotion, etc.
@@ -14,19 +15,11 @@
  * TODO: Different side toggle
  * TODO: save games to PGN file format
  * TODO: if two knights can move to the same square, then the notation should reflect that.
- *
- * TODO: Note that my HashMap of pieces should never delete a piece unless it's captured. Instead, it should set that piece.
- * TODO: Fix the HashMap bug where my moved pieces are still in the map, only empty.
  * TODO: Sometimes when the black king is put in check, the piece attacking it is replaced by a random one and the black king is castled.
+ * TODO: en passant should work.
+ * TODO: kings should be able to capture a piece that's threatening them.
  *
  * TODO: Test case - promoting when it results in check. A good field test for illegal move check.
- *
- * I'll need to have a separate method or something, that goes through every move in the HashMap and tries it out.
- * Then if it causes the king to get in check, I'll make it not possible.
- * Remember: in this method I'm tampering with blackKingInCheck and whiteKingInCheck directly.
- * So I'll need to be careful about what my final value is for those.
- * If it was false initially, it should be false after that too.
- * It should also be reset for every iteration.
  * **/
 
 import javax.swing.*;
@@ -36,7 +29,7 @@ import java.awt.*;
  * Chess piece images from https://commons.wikimedia.org/wiki/Category:PNG_chess_pieces/Standard_transparent
  *
  * @author Gene Yang
- * @version Jan. 19, 2023
+ * @version Jan. 21, 2023
  **/
 
 public class Hackberry {
