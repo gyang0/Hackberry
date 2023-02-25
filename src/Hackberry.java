7d8e434965d2a6d4205f1db24886c8a126465d32
gyang0 <107653576+gyang0@users.noreply.github.com>
2023-02-24 18:24:07 -0800

    Update Hackberry.java

--- src/Hackberry.java
+++ src/Hackberry.java
@@ -1,3 +1,11 @@
+/** Milestones
+ * Jan 4, 2023 - Successfully recreated the Opera Game (Paul Morphy vs Duke of Brunswick & Count Isouard, 1858).
+ * Jan 8, 2023 - All legal moves are now recognized (hopefully), including: En passant, castling, checks, promotion, etc.
+ * Jan 26, 2023 - Optimizing to program is done. All legal moves are recognized as well. ~500K -> ~65K iterations.
+ * Feb 20, 2023 - AI makes random moves. Includes promotion, en passant, and castling.
+ * **/
+
+
 /**
  * This is where most of the stuff will take place.
  * All methods will be combined to set turns, make moves, etc.
@@ -9,8 +17,8 @@
  * TODO: if two knights can move to the same square, then the notation should reflect that. (same for pawns, rooks, queens, bishops)
  * TODO: 50-move rule (no pawn moves or captures), stalemate, checkmate, draw by insufficient material
  * TODO: threefold repetition (this should check the HashMaps, not the board)
- * TODO: Notation should only be updated after move is confirmed to be legal.
  * TODO: pawn values should increase with every step taken.
+ * TODO: make the notation work with captures, castling, en passant, and promotions.
  * **/
 
 import javax.swing.*;
@@ -20,7 +28,7 @@ import java.awt.*;
  * Chess piece images from https://commons.wikimedia.org/wiki/Category:PNG_chess_pieces/Standard_transparent
  *
  * @author Gene Yang
- * @version Feb. 21, 2023
+ * @version Feb. 24, 2023
  **/
 
 public class Hackberry {
