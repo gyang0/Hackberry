06d838a388e862abeb1c6c6a37b862da1d73e737
gyang0 <107653576+gyang0@users.noreply.github.com>
2023-04-19 11:22:42 -0700

    Update HackberryAI.java

--- src/HackberryAI.java
+++ src/HackberryAI.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * AI for user to play against.
- * Makes random moves from the first possible piece.
  *
  * TODO: handle promotion (either a queen or a knight, no need to check others)
+ * TODO: consider candidate moves first
  */
 
 public class HackberryAI {
@@ -71,10 +70,159 @@ public class HackberryAI {
         System.out.println();
     }
 
-    private int numNodes = 0;
-    public double minimax(int fromX, int fromY, int toX, int toY, Piece[][] pieces, int[] mostRecentPieceMov, int[] prevCoords,
-                          boolean whiteTurn, int depth, double alpha, double beta){
-        return 0.0;
+    /*public ArrayList<int[]> getFilteredMoves(ArrayList<int[]> arr, Piece[][] originalPieces){
+        ArrayList<Double> scores = new ArrayList<>(); // {move score, fromX, fromY, toX, toY}
+        HashMap<Double, int[]> scoresToMoves = new HashMap<>();
+
+        for(int[] move : arr){
+            Piece[][] pieces = BoardEval.makeCopy(originalPieces);
+            pieces[move[0]][move[1]].playMove(move[2], move[3], pieces);
+
+            scores.add(BoardEval.boardScore(pieces));
+            scoresToMoves.put(BoardEval.boardScore(pieces), new int[]{move[0], move[1], move[2], move[3]});
+        }
+
+        Collections.sort(scores);
+
+        ArrayList<int[]> ans = new ArrayList<>();
+        for(int i = scores.size() - 1; i >= Math.max(0, scores.size() - NUM_CANDIDATES); i--) {
+            int[] bestMove = scoresToMoves.get(scores.get(i));
+            ans.add(new int[]{bestMove[0], bestMove[1], bestMove[2], bestMove[3]});
+        }
+
+        return ans;
+    }*/
+
+    public ArrayList<int[]> sortByCaptures(ArrayList<int[]> arr, Piece[][] pieces){
+        ArrayList<int[]> ans = new ArrayList<>();
+
+        for(int i = 0; i < arr.size(); i++){
+            if(pieces[arr.get(i)[2]][arr.get(i)[3]].getSide() != ' ')
+                ans.add(new int[]{arr.get(i)[0], arr.get(i)[1], arr.get(i)[2], arr.get(i)[3]});
+        }
+
+        for(int i = 0; i < arr.size(); i++){
+            if(pieces[arr.get(i)[2]][arr.get(i)[3]].getSide() == ' ')
+                ans.add(new int[]{arr.get(i)[0], arr.get(i)[1], arr.get(i)[2], arr.get(i)[3]});
+        }
+
+        return ans;
+    }
+
+    public double minimax(int fromX, int fromY, int toX, int toY, Piece[][] originalPieces,
+                          char curSide, int depth,
+                          double alpha, double beta){
+        // Make a copy and play the move.
+        Piece[][] pieces = BoardEval.makeCopy(originalPieces);
+        pieces[fromX][fromY].playMove(toX, toY, pieces);
+
+        //print(pieces);
+
+        // Update possible moves for the computer
+        ArrayList<int[]>[][] moveGen = BoardEval.updateControlledSquares(pieces);
+
+        // Get the possible moves in this position
+        ArrayList<int[]> movesW = new ArrayList<>();
+        ArrayList<int[]> movesB = new ArrayList<>();
+        for(int i = 0; i < NUM_SQUARES; i++){
+            for(int j = 0; j < NUM_SQUARES; j++){
+                for(int[] arr : moveGen[i][j]){
+                    if(pieces[arr[0]][arr[1]].getSide() == 'w')
+                        movesW.add(new int[]{arr[0], arr[1], i, j});
+                    else
+                        movesB.add(new int[]{arr[0], arr[1], i, j});
+                }
+            }
+        }
+
+        // Reached max depth or no moves to make
+        if(depth == 0) return BoardEval.boardScore(pieces);
+        if(movesW.size() == 0 && movesB.size() == 0) return 0.0; // Stalemate
+        if(curSide == 'w' && movesW.size() == 0) return Integer.MIN_VALUE; // Checkmate by black
+        if(curSide == 'b' && movesB.size() == 0) return Integer.MAX_VALUE; // Checkmate by white
+
+
+        // Sort by captures
+        movesW = sortByCaptures(movesW, pieces);
+        movesB = sortByCaptures(movesB, pieces);
+
+        if(curSide == 'w') {
+            double val = -100000.0;
+
+            // Filter the best moves
+            /*ArrayList<int[]> filteredMovesW = getFilteredMoves(movesW, pieces);
+            for(int[] move : filteredMovesW){
+                System.out.println("    - Filtered candidate: (" + move[0] + ", " + move[1] + ") to (" + move[2] + ", " + move[3] + ")");
+            }*/
+
+            for (int[] move : /*filteredMovesW*/movesW){
+                val = Math.max(val, minimax(move[0], move[1], move[2], move[3], pieces, 'b', depth - 1, alpha, beta));
+
+                if(val > beta)
+                    break;
+                alpha = Math.max(alpha, val);
+            }
+            return val;
+        } else {
+            double val = 100000.0;
+
+            // Filter the best moves
+            /*ArrayList<int[]> filteredMovesB = getFilteredMoves(movesB, pieces);
+            for(int[] move : filteredMovesB){
+                System.out.println("    - Filtered candidate: (" + move[0] + ", " + move[1] + ") to (" + move[2] + ", " + move[3] + ")");
+            }*/
+
+            for(int[] move : /*filteredMovesB*/movesB){
+                val = Math.min(val, minimax(move[0], move[1], move[2], move[3], pieces, 'w', depth - 1, alpha, beta));
+
+                if(val < alpha)
+                    break;
+                beta = Math.min(beta, val);
+            }
+            return val;
+        }
+    }
+
+    public double negamax(Piece[][] originalPieces, int[] move, int depth, double alpha, double beta, int curSide){
+        // Max depth reached
+        if(depth == 0) return BoardEval.boardScore(originalPieces);
+
+        // Make a copy and play the move.
+        Piece[][] pieces = BoardEval.makeCopy(originalPieces);
+        pieces[move[0]][move[1]].playMove(move[2], move[3], pieces);
+
+        // Update possible moves for the computer
+        ArrayList<int[]>[][] moveGen = BoardEval.updateControlledSquares(pieces);
+
+        // Is the game over with the other side to move?
+        if(BoardEval.gameOver(moveGen, pieces, -curSide) != -1)
+            return BoardEval.gameOver(moveGen, pieces, -curSide);
+
+        // Get the possible moves in this position
+        ArrayList<int[]> possibleMoves = new ArrayList<>();
+        for(int i = 0; i < NUM_SQUARES; i++){
+            for(int j = 0; j < NUM_SQUARES; j++){
+                for(int[] arr : moveGen[i][j]){
+                    if((pieces[arr[0]][arr[1]].getSide() == 'w' && curSide == -1) ||
+                       (pieces[arr[0]][arr[1]].getSide() == 'b' && curSide == 1))
+                        possibleMoves.add(new int[]{arr[0], arr[1], i, j});
+                }
+            }
+        }
+
+        // Sort by captures
+        possibleMoves = sortByCaptures(possibleMoves, pieces);
+
+        double val = -10000.0;
+        for(int[] toTest : possibleMoves){
+            val = Math.max(val, -negamax(pieces, toTest, depth - 1, -beta, -alpha, -curSide));
+
+            alpha = Math.max(alpha, val);
+            if(alpha >= beta)
+                break;
+        }
+
+        return val;
     }
 
     // Actual AI goes here
@@ -84,67 +232,61 @@ public class HackberryAI {
         ArrayList<int[]>[][] moveGen = BoardEval.updateControlledSquares(pieces);
 
         // Get list of all possible moves
-        ArrayList<int[]> moves = new ArrayList<>();
+        ArrayList<int[]> movesW = new ArrayList<>();
+        ArrayList<int[]> movesB = new ArrayList<>();
 
         for(int i = 0; i < NUM_SQUARES; i++){
             for(int j = 0; j < NUM_SQUARES; j++){
                 for(int[] arr : moveGen[i][j]){
-                    if(this.side == pieces[arr[0]][arr[1]].getSide())
-                        moves.add(new int[]{arr[0], arr[1], i, j});
+                    if(pieces[arr[0]][arr[1]].getSide() == 'w')
+                        movesW.add(new int[]{arr[0], arr[1], i, j});
+                    else
+                        movesB.add(new int[]{arr[0], arr[1], i, j});
                 }
             }
         }
 
-        int ind = (int)(Math.random() * moves.size());
-        pieces[moves.get(ind)[0]][moves.get(ind)[1]].playMove(moves.get(ind)[2], moves.get(ind)[3], pieces);
-
-
-        //numNodes = 0;
+        //int ind = (int)(Math.random() * moves.size());
+        //pieces[moves.get(ind)[0]][moves.get(ind)[1]].playMove(moves.get(ind)[2], moves.get(ind)[3], pieces);
 
         /*if(this.stillInOpening()){
-            //playOpening();
-            return;
+            playOpening();
         } else {*/
-            // [piece X, piece Y, goal square X, goal square Y]
-            /*int[] best = new int[4];
+            int[] best = new int[4];
+            if(this.side == 'w'){
 
-            if(side == 'w'){
-                double bestScore = Integer.MIN_VALUE;
+                // Get the best move that maximizes the board score
+                double bestScore = -100000.0;
+                for(int[] move : movesW){
+                    System.out.print("Considering move (" + move[0] + ", " + move[1] + ") to (" + move[2] + ", " + move[3] + ") --> ");
+                    double score = negamax(pieces, move, DEPTH, -100000.0, 100000.0, 1);
+                    System.out.println(score);
 
-                for(Piece p : piecesW.keySet()){
-                    for(int arr[] : piecesW.get(p)){
-                        double score = minimax(p.getGridX(), p.getGridY(), arr[0], arr[1], pieces, mostRecentPieceMov, prevCoords, whiteTurn, DEPTH, Integer.MIN_VALUE, Integer.MAX_VALUE);
-
-                        if(score > bestScore) {
-                            bestScore = score;
-                            best = new int[]{p.getGridX(), p.getGridY(), arr[0], arr[1]};
-                        }
+                    if(score > bestScore){
+                        bestScore = score;
+                        best = new int[]{move[0], move[1], move[2], move[3]};
                     }
                 }
-            } else {
-                double bestScore = Integer.MAX_VALUE;
 
-                for(Piece p : piecesB.keySet()){
-                    for(int arr[] : piecesB.get(p)){
-                        System.out.println("Checking " + p + " to (" + arr[0] + ", " + arr[1] + ")");
-                        double score = minimax(p.getGridX(), p.getGridY(), arr[0], arr[1], pieces, mostRecentPieceMov, prevCoords, !whiteTurn, DEPTH, Integer.MIN_VALUE, Integer.MAX_VALUE);
-                        System.out.println("Score: " + score);
+            } else if(this.side == 'b'){
+                double bestScore = 100000.0;
+                for(int[] move : movesB){
+                    System.out.print("Considering move (" + move[0] + ", " + move[1] + ") to (" + move[2] + ", " + move[3] + ") --> ");
+                    double score = negamax(pieces, move, DEPTH, -100000.0, 100000.0, -1);
+                    System.out.println(score);
 
-                        if(score < bestScore) {
-                            bestScore = score;
-                            best = new int[]{p.getGridX(), p.getGridY(), arr[0], arr[1]};
-                        }
+                    if(score < bestScore){
+                        bestScore = score;
+                        best = new int[]{move[0], move[1], move[2], move[3]};
                     }
                 }
             }
-
-
-            System.out.println("\n\nChosen move: " + pieces[best[0]][best[1]] + " to (" + best[2] + ", " + best[3] + ")");
-            System.out.println("Searched " + numNodes + " nodes.");
-            mostRecentPieceMov[0] = best[2];
-            mostRecentPieceMov[1] = best[3];
-            pieces[best[0]][best[1]].playMove(best[2], best[3], pieces);*/
         //}
+
+        // Play the best move found
+        pieces[best[0]][best[1]].playMove(best[2], best[3], pieces);
+
+        System.out.println();
     }
 
 
