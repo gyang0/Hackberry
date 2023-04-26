12dea67b5199d71a6796f9e7d3dc38ed7481189b
gyang0 <107653576+gyang0@users.noreply.github.com>
2023-04-26 06:26:48 -0700

    Update HackberryAI.java

--- src/HackberryAI.java
+++ src/HackberryAI.java
@@ -3,7 +3,7 @@ import java.util.*;
 /**
  * AI for user to play against. Uses simple minimax with alpha-beta pruning to brute-force moves.
  * Takes some positional factors into account, and caches board states to improve speed later on in the game.
- * 
+ *
  * @author Gene Yang
  * @version April 26, 2023
  */
@@ -12,6 +12,18 @@ public class HackberryAI {
     private final int NUM_SQUARES = 8;
     private char side;
     private int DEPTH;
+    
+    public static String[][] continuations = new String[][]{
+            {"e4", "e5", "Nf3", "Nf6", "Bb5", "a6", "Ba4", "Nf6", "O-O"}, // Ruy Lopez
+            {"d4", "d5", "c4", "c6", "Nc3", "e6", "Nf3", "Nf6", "Bg5", "dxc4"}, // Queen's Gambit + slav defense
+            {"d4", "d5", "c4", "Nf3", "Nf6", "Nc3", "c5", "cxd5", "exd5", "Bg5", "Be7", "dxc5"}, // Queen's Gambit Declined
+            {"e4", "e5", "Nf3", "Nf6", "Bc4", "Nf6", "d3", "Bc5", "c3", "O-O", "O-O"}, // Giuoco Piano
+            {"e4", "e5", "Nf3", "Nf6", "Bc4", "Nf6", "Ng5", "d4", "exd5", "Na5"}, // Fried Liver + refutation
+            {"e4", "e5", "Nf3", "Nf6", "d4", "exd4"}, // Scotch Game
+
+            {"e4", "c5", "Nf3", "d3", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "a6", "Be2", "e5"}, // Sicilian, Najdorf Defense
+            {"d4", "Nf6", "c4", "g6", "Nc3", "Bg7", "e4", "d6", "Be2", "O-O"}, // King's Indian Defense
+    };
 
     HashMap<String, Double> boardCache;
 
